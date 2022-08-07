b5aca878b94a2584580a96991ccb979f3d34254e
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-08-06 21:23:53 -0300

    confList

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
+
+  const [ tarefa, setTarefa ] = useState('');
+
+  const [ list, setList] = useState([
+    {
+      key: '1',
+      item: 'Compras'
+    },
+    {
+      key: '2',
+      item: 'React Native'
+    },
+]);
+
+  function handleAdd() {
+    alert(tarefa)
+  }
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>TESTE</Text>
-      <View>
+      <View style={styles.containerInput}>
         <TextInput
         placeholder='Digite sua tarefas'
         style={styles.input}
+        value={tarefa}
+        onChangeText={ (text) => setTarefa(text)}
         />
+
+        <TouchableOpacity style={styles.buttonADD} onPress={handleAdd}>
+          <FontAwesome name="plus" size={24} color="#FFF" />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -28,5 +55,30 @@ const styles = StyleSheet.create({
     marginTop: '5%',
     paddingStart: '5%',
     marginBottom: 12,
+  },
+  containerInput:{
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 22,
+  },
+  input:{
+    width: '75%',
+    backgroundColor: '#fbfbfb',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonADD:{
+    width: '15%',
+    height: 44,
+    backgroundColor: '#73f7ff',
+    marginLeft: 8,
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
+
 });
